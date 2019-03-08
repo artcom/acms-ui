a8b980a07b2a94d06335bc97e9906999505114fb
Niclas Rosso <niclas.rosso@artcom.de>
2019-03-08 14:50:46 +0100

    Adjust checkerboard background

--- src/editors/assetEditor.js
+++ src/editors/assetEditor.js
@@ -27,10 +27,11 @@ function renderView(field, config) {
   const style = { width: "100%" }
 
   const checkerboard = {
-    backgroundImage:
-                `linear-gradient(to right, rgba(192, 192, 192, 0.75), rgba(192, 192, 192, 0.75)),
-                linear-gradient(to right, black 50%, white 50%),
-                linear-gradient(to bottom, black 50%, white 50%)`,
+    width: "100%",
+    backgroundImage: `
+      linear-gradient(to right, rgba(150, 150, 150, 0.75), rgba(150, 150, 150, 0.75)),
+      linear-gradient(to right, black 50%, white 50%),
+      linear-gradient(to bottom, black 50%, white 50%)`,
     backgroundBlendMode: "normal, difference, normal",
     backgroundSize: "2em 2em"
 
