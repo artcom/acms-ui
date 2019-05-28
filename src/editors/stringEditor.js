c88fdd8e933380eadfd4ba9c8107800c13a9606a
Mirko Geissler <mirko.geissler@artcom.de>
2019-05-28 11:52:09 +0200

    Show textarea for string

--- src/editors/stringEditor.js
+++ src/editors/stringEditor.js
@@ -2,5 +2,8 @@ import React from "react"
 import { FormControl } from "react-bootstrap"
 
 export default function StringEditor({ field, onChange }) {
-  return <FormControl type="text" value={ field.value } onChange={ onChange } />
+  return <FormControl
+    componentClass="textarea"
+    value={ field.value }
+    onChange={ onChange } />
 }
