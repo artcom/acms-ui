87cb2fb32a4837a3d1f133624d4ad61583252758
Mirko Geissler <mirko.geissler@artcom.de>
2020-08-11 16:11:24 +0200

    Fix localization modal layout

--- src/modals/fieldLocalizationModal.js
+++ src/modals/fieldLocalizationModal.js
@@ -2,11 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 
 import Button from "react-bootstrap/Button"
-import FormCheck from "react-bootstrap/FormCheck"
-import FormLabel from "react-bootstrap/FormLabel"
 import Form from "react-bootstrap/Form"
-import FormGroup from "react-bootstrap/FormGroup"
-import Badge from "react-bootstrap/Badge"
 import Modal from "react-bootstrap/Modal"
 
 import {
@@ -33,29 +29,22 @@ function FieldLocalizationModal({ dispatch, fieldLocalization, languages }) {
       onHide={ () => dispatch(cancelFieldLocalization()) }>
       <Form>
         <Modal.Header closeButton>
-          <Modal.Title>Localize Field</Modal.Title>
+          <Modal.Title>Languages</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <FormGroup>
-            <FormLabel>Languages</FormLabel>
-            {
-              languages.map((language, index) => {
-                const isDefault = index === 0
-
-                return (
-                  <FormCheck
-                    key={ language.id }
-                    disabled={ isDefault }
-                    checked={ fieldLocalization.languageIds.get(language.id) }
-                    onChange={ event =>
-                      dispatch(updateFieldLocalization(language.id, event.target.checked))
-                    }>
-                    { language.name } { isDefault && <Badge>Default</Badge> }
-                  </FormCheck>
-                )
-              })
-            }
-          </FormGroup>
+          {
+            languages.map((language, index) =>
+              <Form.Check
+                key={ language.id }
+                type="checkbox"
+                disabled={ index === 0 }
+                checked={ fieldLocalization.languageIds.get(language.id) }
+                onChange={ event =>
+                  dispatch(updateFieldLocalization(language.id, event.target.checked))
+                }
+                label={ language.name } />
+            )
+          }
         </Modal.Body>
         <Modal.Footer>
           <Button
