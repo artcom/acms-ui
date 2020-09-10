ade86327b403dcd71fff3f4db19de41e0949bc18
Philipp Niederlag <philipp.niederlag@artcom.de>
2020-09-10 12:09:11 +0200

    Rename application.js to app.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { updateUser } from "./actions/user"
 import bootstrap from "./bootstrap"
 import { configureStore } from "./store"
 
-import Application from "./containers/application"
+import Application from "./containers/app"
 import { showError } from "./actions/error"
 
 import "bootstrap/dist/css/bootstrap.min.css"
