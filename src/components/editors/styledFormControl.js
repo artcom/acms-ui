82cb21be028e96348adeef69f95ea8f5919c7119
Philipp Niederlag <philipp.niederlag@artcom.de>
2020-09-10 16:29:07 +0200

    Fix style of editors

--- src/components/editors/styledFormControl.js
+++ src/components/editors/styledFormControl.js
@@ -1,10 +1,23 @@
+import React from "react"
 import Form from "react-bootstrap/Form"
 import styled from "styled-components"
 
 const StyledFormControl = styled(Form.Control)`
     border: 0px;
     box-shadow: none; 
+    outline: none; 
+    width: 100%;
+    padding: 10px 20px;
+
+    :focus {
+        border: 0px;
+        box-shadow: none; 
+        outline: none; 
+    }
 `
 
-export default StyledFormControl
+export default props =>
+  <StyledFormControl { ...props } >
+    { props.children }
+  </StyledFormControl>
 
