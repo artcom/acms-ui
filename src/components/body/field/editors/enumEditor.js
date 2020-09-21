0da7e92b165075527a8072e65382c96bbe41062a
Mirko Geissler <mirko.geissler@artcom.de>
2020-09-21 08:44:52 +0200

    Make enum icon consistent

--- src/components/body/field/editors/enumEditor.js
+++ src/components/body/field/editors/enumEditor.js
@@ -9,7 +9,7 @@ import StyledFormControl from "./styledFormControl"
 // supported by bootstrap is not working with Styled Components
 const StyledSelectControl = styled(StyledFormControl)`
   appearance: none;
-  background: #fff url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='5' viewBox='0 0 4 5'%3E%3Cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E") no-repeat right .75rem center/8px 10px;
+  background: #fff url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10 10'%3E%3Cpolyline stroke='%23000' stroke-width='2' fill='none' points='1,4 5,8 9,4'/%3E%3C/svg%3E") no-repeat right .75rem center/8px 20px;
 `
 
 export default function EnumEditor({ field, onChange }) {
