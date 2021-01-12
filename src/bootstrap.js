a27a2c19a3df33d95fb3f0e099dde3df5a07b855
Stefan Dieterle <stefand@artcom.de>
2021-01-12 12:33:08 +0100

    Use try/catch block for getting config file

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,14 +4,18 @@ import AssetServer from "./apis/assetServer"
 import ConfigServer from "./apis/configServer"
 
 export default async () => {
-  const {
-    assetServerUri = process.env.ASSET_SERVER_URI,
-    configServerUri = process.env.CONFIG_SERVER_URI,
-    cmsConfigPath = process.env.CMS_CONFIG_PATH
-  } = await axios.get("config.json").then(data => data).catch(() => ({}))
-
-  const assetServer = new AssetServer(assetServerUri)
-  const configServer = new ConfigServer(configServerUri)
-
-  return { assetServer, cmsConfigPath, configServer }
+  try {
+    const config = await axios.get("config.json")
+    const { assetServerUri, configServerUri, cmsConfigPath } = config.data
+    const assetServer = new AssetServer(assetServerUri)
+    const configServer = new ConfigServer(configServerUri)
+    return { assetServer, cmsConfigPath, configServer }
+  } catch (error) {
+    const assetServerUri = process.env.ASSET_SERVER_URI
+    const configServerUri = process.env.CONFIG_SERVER_URI
+    const cmsConfigPath = process.env.CMS_CONFIG_PATH
+    const assetServer = new AssetServer(assetServerUri)
+    const configServer = new ConfigServer(configServerUri)
+    return { assetServer, cmsConfigPath, configServer }
+  }
 }
