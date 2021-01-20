a37352a292e054d3afb78888046d96a1e4d0d8d0
Stefan Dieterle <stefand@artcom.de>
2021-01-20 08:41:49 +0100

    Refactor

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,18 +4,26 @@ import AssetServer from "./apis/assetServer"
 import ConfigServer from "./apis/configServer"
 
 export default async () => {
+  const configFromEnv = {
+    assetServerUri: process.env.ASSET_SERVER_URI,
+    configServerUri: process.env.CONFIG_SERVER_URI,
+    cmsConfigPath: process.env.CMS_CONFIG_PATH
+  }
+
+  const configFromJson = await loadConfigFromJson("config.json")
+  const config = { ...configFromEnv, ...configFromJson }
+
+  const assetServer = new AssetServer(config.assetServerUri)
+  const configServer = new ConfigServer(config.configServerUri)
+
+  return { assetServer, cmsConfigPath: config.cmsConfigPath, configServer }
+}
+
+async function loadConfigFromJson(path) {
   try {
-    const config = await axios.get("config.json")
-    const { assetServerUri, configServerUri, cmsConfigPath } = config.data
-    const assetServer = new AssetServer(assetServerUri)
-    const configServer = new ConfigServer(configServerUri)
-    return { assetServer, cmsConfigPath, configServer }
+    const response = await axios.get(path)
+    return response.data
   } catch (error) {
-    const assetServerUri = process.env.ASSET_SERVER_URI
-    const configServerUri = process.env.CONFIG_SERVER_URI
-    const cmsConfigPath = process.env.CMS_CONFIG_PATH
-    const assetServer = new AssetServer(assetServerUri)
-    const configServer = new ConfigServer(configServerUri)
-    return { assetServer, cmsConfigPath, configServer }
+    return {}
   }
 }
