969313ef773862d907fbbd3b9006a9078bf69aa8
Stefan Dieterle <stefand@artcom.de>
2021-02-08 11:54:06 +0100

    Simplify fetching config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,26 +4,14 @@ import AssetServer from "./apis/assetServer"
 import ConfigServer from "./apis/configServer"
 
 export default async () => {
-  const configFromEnv = {
-    assetServerUri: process.env.ASSET_SERVER_URI,
-    configServerUri: process.env.CONFIG_SERVER_URI,
-    cmsConfigPath: process.env.CMS_CONFIG_PATH
-  }
+  const {
+    assetServerUri = process.env.ASSET_SERVER_URI,
+    configServerUri = process.env.CONFIG_SERVER_URI,
+    cmsConfigPath = process.env.CMS_CONFIG_PATH
+  } = await axios.get("config.json").then(({ data }) => data).catch(() => ({}))
 
-  const configFromJson = await loadConfigFromJson("config.json")
-  const config = { ...configFromEnv, ...configFromJson }
+  const assetServer = new AssetServer(assetServerUri)
+  const configServer = new ConfigServer(configServerUri)
 
-  const assetServer = new AssetServer(config.assetServerUri)
-  const configServer = new ConfigServer(config.configServerUri)
-
-  return { assetServer, cmsConfigPath: config.cmsConfigPath, configServer }
-}
-
-async function loadConfigFromJson(path) {
-  try {
-    const response = await axios.get(path)
-    return response.data
-  } catch (error) {
-    return {}
-  }
+  return { assetServer, cmsConfigPath, configServer }
 }
