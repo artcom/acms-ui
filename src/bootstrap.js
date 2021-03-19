6f69124ba8071b7a30494e35ce8865ac7c79399a
fabian <fabian@artcom.de>
2021-03-19 16:44:40 +0100

    Remove option to configure UI with env variables

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,17 +1,17 @@
-import axios from "axios"
-
 import AssetServer from "./apis/assetServer"
 import ConfigServer from "./apis/configServer"
 
 export default async () => {
-  const {
-    assetServerUri = process.env.ASSET_SERVER_URI,
-    configServerUri = process.env.CONFIG_SERVER_URI,
-    cmsConfigPath = process.env.CMS_CONFIG_PATH
-  } = await axios.get("config.json").then(({ data }) => data).catch(() => ({}))
+  const response = await fetch("config.json")
 
-  const assetServer = new AssetServer(assetServerUri)
-  const configServer = new ConfigServer(configServerUri)
+  if (response.ok) {
+    const { assetServerUri, configServerUri, cmsConfigPath } = await response.json()
 
-  return { assetServer, cmsConfigPath, configServer }
+    const assetServer = new AssetServer(assetServerUri)
+    const configServer = new ConfigServer(configServerUri)
+
+    return { assetServer, cmsConfigPath, configServer }
+  } else {
+    throw new Error(response.statusText)
+  }
 }
