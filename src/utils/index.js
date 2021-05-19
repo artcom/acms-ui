f9dcd4ba9240e207f3480814759bee6c62184e55
Mirko Geissler <mirko.geissler@artcom.de>
2021-05-19 14:22:26 +0200

    Return default if template is unknown

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,12 +4,22 @@ import isNumber from "lodash/isNumber"
 import isUndefined from "lodash/isUndefined"
 import isPlainObject from "lodash/isPlainObject"
 
+const DEFAULT_TEMPLATE = { fields: [], fixedChildren: [], children: [] }
+
 export function getFromPath(object, path, defaultValue) {
   return path.length === 0 ? object : get(object, path, defaultValue)
 }
 
 export function getTemplate(id, templates) {
-  return templates[id] ? templates[id] : templates[`${id}/index`]
+  if (templates[id]) {
+    return { ...DEFAULT_TEMPLATE, ...templates[id] }
+  }
+
+  if (templates[`${id}/index`]) {
+    return { ...DEFAULT_TEMPLATE, ...templates[`${id}/index`] }
+  }
+
+  return DEFAULT_TEMPLATE
 }
 
 export function createEntry(entry, templates) {
