865da3b5934065207c3f3ee35d7c6f7f9a8bcddd
Dino Lindlau <dino.lindlau@artcom.de>
2022-01-14 11:11:40 +0100

    Refactor code with redux-toolkit createReducer

--- src/reducers.js
+++ src/reducers.js
@@ -4,32 +4,65 @@ import { createReducer } from "@reduxjs/toolkit"
 import get from "lodash/get"
 import set from "lodash/set"
 import unset from "lodash/unset"
-
 import resolveConfig from "./resolveConfig"
 
 export const config = createReducer(null, {
   UPDATE_DATA: (state, { payload }) => resolveConfig(payload.config),
 })
 
-export const isSaving = createReducer(false, {
-  START_SAVING: () => true,
-  UPDATE_DATA: () => false,
-  SHOW_ERROR: () => false,
+export const user = createReducer(null, builder => {
+  builder.addCase("UPDATE_USER", (state, action) => action.payload.user)
 })
 
-export const version = createReducer(null, {
-  UPDATE_DATA: (state, { payload }) => payload.version,
+export const isSaving = createReducer(false, builder => {
+  builder.addCase("START_SAVING", () => true)
+  builder.addCase("UPDATE_DATA", () => false)
+  builder.addCase("SHOW_ERROR", () => false)
 })
 
-export const templates = createReducer(null, {
-  UPDATE_DATA: (state, { payload }) => payload.templates,
+export const version = createReducer(null, builder => {
+  builder.addCase("UPDATE_DATA", (state, action) => action.payload.version)
 })
 
-export const originalContent = createReducer(null, {
-  UPDATE_DATA: (state, { payload }) => payload.originalContent,
+export const templates = createReducer(null, builder => {
+  builder.addCase("UPDATE_DATA", (state, action) => action.payload.templates)
 })
 
-export const changedContent = produce((draft, { type, payload }) => {
+export const path = createReducer(null, builder => {
+  builder.addCase("UPDATE_PATH", (state, action) => action.payload.path)
+})
+
+export const flash = createReducer(null, builder => {
+  builder
+    .addCase("SHOW_ERROR", (state, action) => action.payload)
+    .addCase("HIDE_ERROR", () => null)
+})
+
+export const originalContent = createReducer(null, builder => {
+  builder.addCase("UPDATE_DATA", (state, action) => action.payload.originalContent)
+})
+
+// testing and code
+export const changedContent = createReducer(null, builder => {
+  builder
+    .addCase("UPDATE_DATA", (state, action) => action.payload.changedContent)
+    .addCase("SET_VALUE", (state, action) => {
+      set(state, action.payload.path, action.payload.value)
+    })
+    .addCase("UNDO_CHANGES", (state, action) => {
+      set(state, action.payload.path, action.payload.value)
+    })
+    .addCase("FINISH_ENTITY_CREATION", (state, action) => {
+      set(state, action.payload.path, action.payload.value)
+    })
+    .addCase("FINISH_ENTITY_RENAMING", (state, action) => {
+      set(state, action.payload.path, action.payload.value)
+    })
+    .addCase("DELETED_ENTITY", (state, action) => { unset(state, action.payload.path) })
+})
+
+// old to check new one works
+export const changedContentOld = produce((draft, { type, payload }) => {
   switch (type) {
     case "UPDATE_DATA":
       return payload.changedContent
@@ -55,21 +88,14 @@ export const changedContent = produce((draft, { type, payload }) => {
   }
 }, null)
 
-export const changedContentReducer = createReducer(null, {
-  UPDATE_DATA: (state, { payload }) => payload.changedContent,
-  SET_VALUE: (state, { payload }) => set(original(state), payload.path, payload.value),
-  UNDO_CHANGES: (state, { payload }) => set(original(state), payload.path, payload.originalValue),
-  FINISH_ENTITY_CREATION: (state, { payload }) =>
-    set(original(state), payload.path, payload.values),
-  FINISH_ENTITY_RENAMING: (state, { payload }) => {
-    const oldPath = [...payload.path, payload.oldId]
-    const newPath = [...payload.path, payload.newId]
-    set(original(state), newPath, get(original(state), oldPath))
-    unset(original(state), oldPath)
-  },
-  DELETE_ENTITY: (state, { payload }) => unset(original(state), payload.path),
+// test reducer
+export const newEntityReducer = createReducer(null, builder => {
+  builder.addCase("START_ENTITY_CREATION", (state, action) => action.payload.path)
+  builder.addCase("UPDATE_ENTITY_CREATION", (state, action) =>
+    Object.entries(action.payload).forEach(([key, value]) => { state[key] = value }))
+  builder.addCase("FINISH_ENTITY_CREATION", () => null)
+  builder.addCase("CANCEL_ENTITY_CREATION", () => null)
 })
-
 
 export const newEntity = produce((draft, { type, payload }) => {
   switch (type) {
@@ -85,11 +111,13 @@ export const newEntity = produce((draft, { type, payload }) => {
   }
 }, null)
 
-export const newEntityReducer = createReducer(null, {
-  START_ENTITY_CREATION: (state, { payload }) => payload,
-  UPDATE_ENTITY_CREATION: (state, { payload }) => payload,
-  FINISH_ENTITY_CREATION: () => null,
-  CANCEL_ENTITY_CREATION: () => null,
+
+// test reducer
+export const renamedEntityReducer = createReducer(null, {
+  START_ENTITY_RENAMING: (state, { payload }) => payload,
+  UPDATE_ENTITY_RENAMING: (state, { payload }) => ({ ...state, newId: payload.newId }),
+  FINISH_ENTITY_RENAMING: () => null,
+  CANCEL_ENTITY_RENAMING: () => null,
 })
 
 export const renamedEntity = produce((draft, { type, payload }) => {
@@ -106,40 +134,21 @@ export const renamedEntity = produce((draft, { type, payload }) => {
   }
 }, null)
 
-export const renamedEntityReducer = createReducer(null, {
-  START_ENTITY_RENAMING: (state, { payload }) => payload,
-  UPDATE_ENTITY_RENAMING: (state, { payload }) => ({ ...state, newId: payload.newId }),
-  FINISH_ENTITY_RENAMING: () => null,
-  CANCEL_ENTITY_RENAMING: () => null,
+// test needed
+export const progressReducer = createReducer(null, builder => {
+  builder
+    .addCase("START_UPLOAD", (state, action) => {
+      state[action.payload.path.toString()] = 0
+    })
+    .addCase("PROGRESS_UPLOAD", (state, action) => {
+      state[action.payload.path.toString()] = action.payload.progress
+    })
+    .addCase("CANCEL_UPLOAD", (state, action) => {
+      delete state[action.payload.path.toString()]
+    })
 })
 
-
-export const path = produce((draft, { type, payload }) => {
-  switch (type) {
-    case "UPDATE_PATH":
-      return payload.path
-  }
-}, [])
-
-export const pathReducer = createReducer([], {
-  UPDATE_PATH: (state, { payload }) => payload.path,
-})
-
-export const flash = produce((draft, { type, payload }) => {
-  switch (type) {
-    case "SHOW_ERROR":
-      return payload
-    case "HIDE_ERROR":
-      return null
-  }
-}, null)
-
-export const flashReducer = createReducer(null, {
-  SHOW_ERROR: (state, { payload }) => payload,
-  HIDE_ERROR: () => null,
-})
-
-
+// old to check new one works
 export const progress = produce((draft, { type, payload }) => {
   switch (type) {
     case "START_UPLOAD":
@@ -155,28 +164,3 @@ export const progress = produce((draft, { type, payload }) => {
   }
 }, {})
 
-export const progressReducer = createReducer({}, {
-  START_UPLOAD: (state, { payload }) => ({ ...state, [payload.path.toString()]: 0 }),
-  PROGRESS_UPLOAD: (state, { payload }) =>
-    ({ ...state, [payload.path.toString()]: payload.progress }),
-  SET_VALUE: (state, { payload }) => {
-    delete state[payload.path.toString()]
-    return state
-  },
-  CANCEL_UPLOAD: (state, { payload }) => {
-    delete state[payload.path.toString()]
-    return state
-  },
-})
-
-
-export const user = produce((draft, { type, payload }) => {
-  switch (type) {
-    case "UPDATE_USER":
-      return payload.user
-  }
-}, null)
-
-export const userReducer = createReducer(null, {
-  UPDATE_USER: (state, { payload }) => payload.user,
-})
