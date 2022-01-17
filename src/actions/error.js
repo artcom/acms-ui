9eef0531b7b599ffc9f912f0a615ad5ebd01c497
Dino Lindlau <dino.lindlau@artcom.de>
2022-01-17 18:44:29 +0100

    Cleanup

--- src/actions/error.js
+++ src/actions/error.js
@@ -1,7 +1,5 @@
 export function showError(title, details) {
   window.scrollTo(0, 0) // reset scroll position
-  console.log("title in showError: ", title)
-
   const message = details.message
 
   return {
