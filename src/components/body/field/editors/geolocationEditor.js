7a1e3092929c0097179354cd8e0080b5e82f9868
Anton Troll <anton.troll@artcom.de>
2022-03-17 13:10:26 +0100

    Create NumberInput and add it to Geolocation and Number editor

--- src/components/body/field/editors/geolocationEditor.js
+++ src/components/body/field/editors/geolocationEditor.js
@@ -1,55 +1,29 @@
-import React, { useState } from "react"
-import Form from "react-bootstrap/Form"
-import StyledFormControl from "./styledFormControl"
+import React from "react"
+import styled from "styled-components"
+import Card from "react-bootstrap/Card"
+import NumberInput from "./inputs/NumberInput"
+
+const StyledCardLabel = styled(Card.Header)`
+  display: flex;
+  border: 0px;
+`
+
+const Flexbox = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 export default function GeolocationEditor({ field, onChange }) {
-  const [stringValue, setStringValue] = useState(field.value.toString())
-  const min = field.path[field.path.length - 1] === "lon" ? -180.00 : -90.00
-  const max = field.path[field.path.length - 1] === "lon" ? 180.00 : 90.00
-  const regex = /^-?((\d+([.,]\d*)?))?$/
-  const valid = field.value >= min && field.value <= max &&
-    !(field.integer && (stringValue.includes(",") || stringValue && stringValue.includes(".")))
-
-  const onChangeString = event => {
-    if (regex.test(event.target.value) || event.target.value === "") {
-      setStringValue(event.target.value)
-      const finalString = fill(event.target.value.replace(",", "."))
-      let floatGeolocation = parseFloat(finalString).toFixed(finalString.split(".")[1].length)
-      if (event.target.value === "-" || event.target.value === "" || event.target.value === "-.") {
-        floatGeolocation = 0
-      }
-      onChange({ target: { value: floatGeolocation } })
-    }
-  }
-
-  const onBlurString = () => {
-    setStringValue(field.value.toString())
-  }
-
   return (
-    <>
-      <StyledFormControl
-        isInvalid={ !valid }
-        type="text"
-        value={ stringValue }
-        onChange={ onChangeString }
-        onBlur={ onBlurString } />
-      <Form.Control.Feedback type="invalid" tooltip>
-        { `This number should be between ${min} and ${max}` }
-      </Form.Control.Feedback>
-    </>
+    <Flexbox>
+      <StyledCardLabel className="card-text">Latitude</StyledCardLabel>
+      <NumberInput
+        field={ { value: field.value.lat, min: -90, max: 90 } }
+        onChange={ newLat => onChange({ lat: newLat, lon: field.value.lon }) } />
+      <StyledCardLabel className="card-text">Longitude</StyledCardLabel>
+      <NumberInput
+        field={ { value: field.value.lon, min: -180, max: 180 } }
+        onChange={ newLon => onChange({ lat: field.value.lat, lon: newLon }) } />
+    </Flexbox>
   )
-}
-
-function fill(value) {
-  const split = value.split(".")
-  if (split.length === 1) {return `${value}.000000`}
-  if (split[1].length >= 6) {return value}
-
-  let newValue = value
-  for (let i = 0; i < 6 - split[1].length; i++) {
-    newValue += "0"
-  }
-
-  return newValue
 }
