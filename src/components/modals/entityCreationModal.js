ba0b2d20273e89862d8185f36b4a7f987b7ce8b6
Anton Troll <84706588+tone7@users.noreply.github.com>
2022-04-04 21:57:15 +0200

    Show current template type in header if multiple exists
    * Change template-type position into breadcrumbs
    
    * Show template type only if there are multiple children-templates possible
    
    * Fix condition
    
    * Show template type at entityCreationModal
    
    * Create entity on enter
    
    * Rename entity on enter
    
    * Add autofocus to animated modals
    
    Co-authored-by: Dino Lindlau <dino.lindlau@artcom.de>

--- src/components/modals/entityCreationModal.js
+++ src/components/modals/entityCreationModal.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 import Modal from "react-bootstrap/Modal"
 import StyledFormControl from "./styledFormControl"
+
 
 import { cancelEntityCreation,
   finishEntityCreation,
@@ -16,47 +17,57 @@ import { selectNewEntity } from "../../selectors"
 export default function EntityCreationModal() {
   const dispatch = useDispatch()
   const newEntity = useSelector(selectNewEntity)
+  const inputField = useRef()
+
   return (
-    <Modal show={ newEntity.isVisible } onHide={ () => dispatch(cancelEntityCreation()) }>
+    <Modal
+      onShow={ () => inputField.current.focus() }
+      show={ newEntity.isVisible }
+      onHide={ () => dispatch(cancelEntityCreation()) }>
       <Modal.Header closeButton>
         <Modal.Title>New Item</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <Form.Label>Name</Form.Label>
-        <div style={ { position: "relative" } }>
-          <StyledFormControl
-            type="text"
-            value={ newEntity.id }
-            isInvalid={ newEntity.isVisible && !newEntity.isValidId }
-            autoFocus
-            onChange={
-              event => dispatch(updateEntityCreationId(event.target.value))
-            } />
-        </div>
-        {
-          newEntity.templates.length > 1 &&
+      <Form>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <div style={ { position: "relative" } }>
+              <StyledFormControl
+                ref={ inputField }
+                type="text"
+                value={ newEntity.id }
+                isInvalid={ newEntity.isVisible && !newEntity.isValidId }
+                onChange={ event => dispatch(updateEntityCreationId(event.target.value)) } />
+            </div>
+          </Form.Group>
           <Form.Group>
             <Form.Label>Template</Form.Label>
-            <Form.Control
-              as="select"
-              value={ newEntity.template }
-              onChange={ event => dispatch(updateEntityCreationTemplate(event.target.value)) }>
-              { newEntity.templates.map(template =>
-                <option key={ template } value={ template }>{ template }</option>
-              ) }
-            </Form.Control>
+            { newEntity.templates.length === 1 ?
+              <Form.Control
+                readOnly
+                value={ newEntity.templates[0] } />
+              :
+              <Form.Control
+                as="select"
+                value={ newEntity.template }
+                onChange={ event => dispatch(updateEntityCreationTemplate(event.target.value)) }>
+                { newEntity.templates.map(template =>
+                  <option key={ template } value={ template }>{ template }</option>
+                ) }
+              </Form.Control>
+            }
           </Form.Group>
-        }
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          type="submit"
-          variant="info"
-          disabled={ !newEntity.isValidId }
-          onClick={ event => { event.preventDefault(); dispatch(finishEntityCreation()) } }>
-          Create
-        </Button>
-      </Modal.Footer>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            type="submit"
+            variant="info"
+            disabled={ !newEntity.isValidId }
+            onClick={ event => { event.preventDefault(); dispatch(finishEntityCreation()) } }>
+            Create
+          </Button>
+        </Modal.Footer>
+      </Form>
     </Modal>
   )
 }
