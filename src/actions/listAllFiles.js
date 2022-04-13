3013c90b84569f63ccac32b3b0ff26b60df5111e
Dimitar Ruszev <dimitar.ruszev@artcom.de>
2022-04-13 13:26:30 +0200

    merge again

--- src/actions/listAllFiles.js
+++ src/actions/listAllFiles.js
@@ -3,12 +3,7 @@ export function listAllFiles(acmsAssets) {
   return async () => {
     try {
       const files = await acmsAssets.listAllFiles()
-<<<<<<< HEAD
-
-      console.log("ALL FILES", files)
-=======
       return files
->>>>>>> 47a245519cd186aea229d34e734b097dc2cce9ba
     } catch (error) {
       console.error(error)
     }
