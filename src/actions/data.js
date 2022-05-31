e37fa4c29eaf28c89f0576d5fe40e3a5d6958725
Anton Troll <anton.troll@artcom.de>
2022-05-31 13:36:40 +0200

    Cleanup

--- src/actions/data.js
+++ src/actions/data.js
@@ -14,13 +14,10 @@ export function loadData(acmsApi, acmsConfigPath) {
         acmsApi.queryFiles(config.templatesPath, version),
         acmsApi.queryJson(config.contentPath, version),
       ])
-      console.log("🚀 ~ originalContent", originalContent)
 
       const changedContent = createNextState(originalContent, (draft) =>
         fixContent(originalContent, draft, templates)
       )
-
-      console.log("🚀 ~ changedContent", changedContent)
 
       dispatch({
         type: "UPDATE_DATA",
