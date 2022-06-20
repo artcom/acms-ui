8cb0013f5e29a07e5ac3ef272c88e2c858ac55ae
Anton Troll <anton.troll@artcom.de>
2022-06-20 14:54:12 +0200

    WIP: static file path

--- src/components/body/field/requirements/imageRequirements.js
+++ src/components/body/field/requirements/imageRequirements.js
@@ -1,6 +1,8 @@
 import OverlayTrigger from "react-bootstrap/OverlayTrigger"
 import Popover from "react-bootstrap/Popover"
 
+import path from "path-browserify"
+
 import { InfoLg } from "react-bootstrap-icons"
 
 const ImageRequirements = ({ field }) => {
@@ -13,56 +15,81 @@ const ImageRequirements = ({ field }) => {
     field.maxWidth ||
     field.aspectRatio
 
-  if (!hasRequirements) {
+  const hasInfos = field.value.hashedPath !== "" || field.value.lastModified !== ""
+
+  if (!hasRequirements && !hasInfos) {
     return null
   }
   const Tooltip = (
     <Popover>
-      <Popover.Header>Image Requirements</Popover.Header>
-      <Popover.Body>
-        {field.height && (
-          <>
-            {" "}
-            Height: {field.height} <br />{" "}
-          </>
-        )}
-        {field.minHeight && (
-          <>
-            {" "}
-            Min. Height: {field.minHeight} <br />{" "}
-          </>
-        )}
-        {field.maxHeight && (
-          <>
-            {" "}
-            Max. Height: {field.maxHeight} <br />{" "}
-          </>
-        )}
-        {field.width && (
-          <>
-            {" "}
-            Width: {field.width} <br />{" "}
-          </>
-        )}
-        {field.minWidth && (
-          <>
-            {" "}
-            Min. Width: {field.minWidth} <br />{" "}
-          </>
-        )}
-        {field.maxWidth && (
-          <>
-            {" "}
-            Max. Width: {field.maxWidth} <br />{" "}
-          </>
-        )}
-        {field.aspectRatio && (
-          <>
-            {" "}
-            Aspect Ratio: {field.aspectRatio} <br />{" "}
-          </>
-        )}
-      </Popover.Body>
+      {hasRequirements && (
+        <div>
+          <Popover.Header>Image Requirements</Popover.Header>
+          <Popover.Body>
+            {field.height && (
+              <>
+                {" "}
+                Height: {field.height} <br />{" "}
+              </>
+            )}
+            {field.minHeight && (
+              <>
+                {" "}
+                Min. Height: {field.minHeight} <br />{" "}
+              </>
+            )}
+            {field.maxHeight && (
+              <>
+                {" "}
+                Max. Height: {field.maxHeight} <br />{" "}
+              </>
+            )}
+            {field.width && (
+              <>
+                {" "}
+                Width: {field.width} <br />{" "}
+              </>
+            )}
+            {field.minWidth && (
+              <>
+                {" "}
+                Min. Width: {field.minWidth} <br />{" "}
+              </>
+            )}
+            {field.maxWidth && (
+              <>
+                {" "}
+                Max. Width: {field.maxWidth} <br />{" "}
+              </>
+            )}
+            {field.aspectRatio && (
+              <>
+                {" "}
+                Aspect Ratio: {field.aspectRatio} <br />{" "}
+              </>
+            )}
+          </Popover.Body>
+        </div>
+      )}
+      {hasInfos && (
+        <div>
+          <Popover.Header>Image Infos</Popover.Header>
+          <Popover.Body>
+            {field.value.hashedPath !== "" && (
+              <>
+                {" "}
+                Name: {path.basename(field.value.hashedPath).split("-")[0]} <br />{" "}
+              </>
+            )}
+            {field.value.lastModified !== "" && (
+              <>
+                {" "}
+                Last modified: {field.value.lastModified} <br />{" "}
+              </>
+            )}
+          </Popover.Body>
+        </div>
+      )}
     </Popover>
   )
 
